86f0914f102f1b415038ea9b03c38ce8ce435f08
peanutwall <57250847+peanutwall@users.noreply.github.com>
2019-11-21 15:14:30 +0800

    Add files via upload

--- filterWarGame2.py
+++ filterWarGame2.py
@@ -14,6 +14,7 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 GOLD = (255, 215, 0)
+GREEN = (0, 255, 0)
 
 
 class PrintPreface:
@@ -30,8 +31,8 @@ class PrintPreface:
         self.screen.blit(preface, (self.length / 2 - 80, self.width / 5))
         preface2 = fontPreface.render('This area to put some guidance for subjects', True, BLACK)
         self.screen.blit(preface2, (self.length / 2 - 170, self.width / 5 + 30))
-        preface3 = fontPreface.render('This area is reserved for subjects to play the game', True, BLACK)
-        self.screen.blit(preface3, (self.length / 2 - 200, 2 * self.width / 3))
+        # preface3 = fontPreface.render('This area is reserved for subjects to play the game', True, BLACK)
+        # self.screen.blit(preface3, (self.length / 2 - 200, 2 * self.width / 3))
         return self.screen
 
 
@@ -56,10 +57,13 @@ class DrawCubes:
             pygame.draw.rect(self.screen, BLACK, (
             self.length / 2 - mapSize * cubeWidth - cubeWidth / 2 + cubeWidth * i, self.width / 2 - cubeWidth, cubeWidth,
             cubeWidth), 1)
+
         for i in range((mapSize + 1) * 2 - isEven - 1):
             positionAnnotation = fontAnnotations.render(str(i - mapSize + isEven * (i >= mapSize)), False, BLACK)
             self.screen.blit(positionAnnotation,
                         (self.length / 2 - mapSize * cubeWidth + cubeWidth * i, self.width / 2 - 1.5 * cubeWidth))
+
+
         if isBoundary:
             pygame.draw.line(self.screen, GOLD, (self.length / 2, self.width / 2 - cubeWidth), (self.length / 2, self.width / 2), 2)
         return self.screen
@@ -93,7 +97,7 @@ class DrawBases:
             self.screen.blit(baseB, (int(self.length / 2 - mapSize * cubeWidth + (2 * mapSize - isEven) * cubeWidth) - 8,
                                 int(self.width / 2 - cubeWidth / 2) - 8))
         else:
-            pygame.draw.circle(screen, RED, (
+            pygame.draw.circle(self.screen, RED, (
                 int(self.length / 2 - mapSize * cubeWidth + (2 * mapSize - baseLocationRandom - isEven) * cubeWidth),
                 int(self.width / 2 - cubeWidth / 2)), radius)
             baseB = fontBase.render('B', True, WHITE)
@@ -101,3 +105,93 @@ class DrawBases:
                 int(self.length / 2 - mapSize * cubeWidth + (2 * mapSize - baseLocationRandom - isEven) * cubeWidth) - 8,
                 int(self.width / 2 - cubeWidth / 2) - 8))
         return self.screen
+
+
+class DrawWarField:
+
+    def __init__(self, screen, length, width):
+        self.screen = screen
+        self.length = length
+        self.width = width
+
+    def __call__(self, warField, mapSize, cubeWidth, isEven, typingPositionA, typingPositionB):
+        for i in range(mapSize * 2 - isEven - 1):
+            if warField[i] == 0:
+                pygame.draw.circle(self.screen, GOLD, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i+1)),
+                                                   int(self.width / 2 - cubeWidth / 2 + 2*cubeWidth)), radius)
+            elif warField[i] == 1:
+                pygame.draw.circle(self.screen, RED, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)),
+                                                       int(self.width / 2 - cubeWidth / 2 + 2 * cubeWidth)), radius)
+            elif warField[i] == 2:
+                pygame.draw.circle(self.screen, BLUE, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)),
+                                                       int(self.width / 2 - cubeWidth / 2 + 2 * cubeWidth)), radius)
+            if typingPositionA == i:
+                pygame.draw.circle(self.screen, GREEN, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i+1)),
+                                                   int(self.width / 2 - cubeWidth / 2 + 3*cubeWidth)), radius)
+            else:
+                pygame.draw.circle(self.screen, RED,
+                                   (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)),int(self.width / 2 - cubeWidth / 2 + 3 * cubeWidth)), radius)
+            if typingPositionB == i:
+                pygame.draw.circle(self.screen, GREEN, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i+1)),
+                                    int(self.width / 2 - cubeWidth / 2 + 4*cubeWidth)), radius)
+            else:
+                pygame.draw.circle(self.screen, BLUE, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)),
+                                                       int(self.width / 2 - cubeWidth / 2 + 4 * cubeWidth)), radius)
+
+
+class DrawPolicy:
+
+    def __init__(self, screen, length, width):
+        self.screen = screen
+        self.length = length
+        self.width = width
+
+    def __call__(self, policyA, policyB, mapSize, cubeWidth):
+        fontAnnotations = pygame.font.SysFont('Times New Roman', 20)
+        for i in range(len(policyA)):
+            policyFont = fontAnnotations.render(str(policyA[i]), True, WHITE)
+            self.screen.blit(policyFont, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)-8),
+                                          int(self.width / 2 - cubeWidth / 2 + 3*cubeWidth)-8))
+            policyFont = fontAnnotations.render(str(policyB[i]), True, WHITE)
+            self.screen.blit(policyFont, (int(self.length / 2 - mapSize * cubeWidth + cubeWidth * (i + 1)-8),
+                                          int(self.width / 2 - cubeWidth / 2 + 4 * cubeWidth)-8))
+
+
+def generatePositionListA(length, width, mapSize, isEven, cubeWidth):
+    positionList = []
+    for i in range(mapSize * 2 - isEven - 1):
+        positionList.append((int(length / 2 - mapSize * cubeWidth + cubeWidth * (i+1)), int(width / 2 - cubeWidth / 2 + 3*cubeWidth)))
+    return  positionList
+
+def generatePositionListB(length, width, mapSize, isEven, cubeWidth):
+    positionList = []
+    for i in range(mapSize * 2 - isEven - 1):
+        positionList.append((int(length / 2 - mapSize * cubeWidth + cubeWidth * (i+1)), int(width / 2 - cubeWidth / 2 + 4*cubeWidth)))
+    return  positionList
+
+def comparePosition(pos, positionList):
+    limit = radius
+    for i in range(len(positionList)):
+        if abs(positionList[i][0]-pos[0]) <= limit and abs(positionList[i][1]-pos[1]) <= limit:
+            return i
+    return -1
+
+def transformKey(key):
+    return key - 48
+
+def transformList(tempList):
+    number = '0'
+    for i in range(len(tempList)):
+        number = number + str(tempList[i])
+    return int(number)
+
+def judgeResult(policyA, policyB):
+    warField = []
+    for i in range(len(policyA)):
+        warField.append(0)
+    for i in range(len(warField)):
+        if policyA[i] >= policyB[i]:
+            warField[i] = 1
+        elif policyA[i] < policyB[i]:
+            warField[i] = 2
+    return warField
--- warGame2.py
+++ warGame2.py
@@ -5,10 +5,9 @@ import random
 
 from pygame.locals import *
 from sys import exit
-from filterWarGame import *
 from filterWarGame2 import *
 
-mapSize = 4
+mapSize = 3
 isBoundary = 1
 isEven = 0
 isSymmetrical = 1
@@ -19,6 +18,7 @@ length = 800
 cubeWidth = 60
 radius = 25
 landList = list(range(-mapSize, mapSize+1, 1))
+initialSoldiers = 30
 backgroundColor = (255, 250, 240)
 screenSize = (length, width)
 BLACK = (0, 0, 0)
@@ -35,17 +35,80 @@ pygame.display.set_caption("War Game")
 drawCubes = DrawCubes(screen, length, width)
 drawBases = DrawBases(screen, length, width)
 printPreface = PrintPreface(screen, length, width)
+drawWarField = DrawWarField(screen, length, width)
+drawPolicy = DrawPolicy(screen, length, width)
 # printPreface(screen, length, width)
-
+isTypingA = 0
+isTypingB = 0
+typingPositionA = -1
+typingPositionB = -1
+position = -1
+positionListA = []
+positionListB = []
+policyA = []
+policyB = []
+for i in range(mapSize * 2 - isEven - 1):
+    policyA.append(0)
+    policyB.append(0)
+warField = []
+for i in range(mapSize * 2 - isEven - 1):
+    warField.append(0)
 printPreface()
+tempList = []
 
 while True:
+    fontAnnotations = pygame.font.SysFont('Times New Roman', 20)
+    drawCubes(mapSize, isEven, cubeWidth, isBoundary)
+    drawBases(mapSize, cubeWidth, isSymmetrical, isEven, baseLocationRandom)
+    drawWarField(warField, mapSize, cubeWidth, isEven, typingPositionA, typingPositionB)
+    drawPolicy(policyA, policyB, mapSize, cubeWidth)
+    positionListA = generatePositionListA(length, width, mapSize, isEven, cubeWidth)
+    positionListB = generatePositionListB(length, width, mapSize, isEven, cubeWidth)
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
-    fontAnnotations = pygame.font.SysFont('Times New Roman', 20)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONUP:
+            typingPositionA = comparePosition(pos, positionListA)
+            typingPositionB = comparePosition(pos, positionListB)
+            # print(typingPositionA)
+            # print(typingPositionB)
+            if typingPositionA != -1:
+                isTypingA = 1
+            if typingPositionB != -1:
+                isTypingB = 1
+        if event.type == pygame.KEYDOWN and isTypingA:
+            if 48 <= event.key <= 57:
+                tempList.append(transformKey(event.key))
+            if event.key == pygame.K_SPACE and isTypingA:
+                policyA[typingPositionA] = transformList(tempList)
+                # print(tempList)
+                tempList = []
+                # print(policyA[typingPositionA])
+                isTypingA = 0
+                typingPositionA = -1
+        if event.type == pygame.KEYDOWN and isTypingB:
+            if 48 <= event.key <= 57:
+                tempList.append(transformKey(event.key))
+            if event.key == pygame.K_SPACE and isTypingB:
+                policyB[typingPositionB] = transformList(tempList)
+                # print(tempList)
+                tempList = []
+                # print(policyB[typingPositionB])
+                isTypingB = 0
+                typingPositionB = -1
+        if event.type == pygame.KEYDOWN and not isTypingB and not isTypingA:
+            if event.key == pygame.K_RETURN:
+                if sum(policyA) <= initialSoldiers and sum(policyB) <= initialSoldiers:
+                    warField = judgeResult(policyA, policyB)
+                    # print(warField)
 
-    drawCubes(mapSize, isEven, cubeWidth, isBoundary)
 
-    drawBases(mapSize, cubeWidth, isSymmetrical, isEven, baseLocationRandom)
+
+
+
+
+
     pygame.display.update()
+
